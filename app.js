777723caddc360ad377abe2bd215a14d72713656
rohailkhattak66 <153583389+rohailkhattak66@users.noreply.github.com>
2023-12-20 15:55:20 +0500

    v5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js with Express! This is v4 to test polling');
+  res.send('Hello, Node.js with Express! This is v5 to test polling and if it works with a stopped docker');
 });
 
 app.listen(PORT, () => {
